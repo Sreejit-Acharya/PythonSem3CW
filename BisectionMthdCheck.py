63dbe510786a5817678a94f5ffe131580ac64a3e
Sreejit-Acharya <sreejitacharya10@gmail.com>
2025-08-26 15:56:56 +0530

    Add files via upload

---
+++ BisectionMthdCheck.py
@@ -0,0 +1,12 @@
+eps=0.001
+def fn(x):
+    return x**2-5*x+6
+a=int(input("Enter value of x: "))
+res1=fn(a)
+print("Value of f(X) at ",a,"is ",res1)
+b=int(input("Enter value of x: "))
+res2=fn(b)
+print("Value of f(X) at ",b,"is ",res2)
+c=int(input("Enter value of x: "))
+res3=fn(c)
+print("Value of f(X) at ",c,"is ",res3)
